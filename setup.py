6ca9fc391d81e4c5e6f9c2b9433fbd383aa259f7
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-29 13:23:06 +0200

    Make mypy recognize this packages typing

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 test_deps = [
@@ -14,16 +14,17 @@ extras = {
 }
 
 setuptools.setup(
-    name="text-formatter",
-    version="0.0.2",
-    author="Ruben Lipperts",
-    author_email="",
-    description="Simple text output formatter that helps with printing pretty stuff",
+    name='text-formatter',
+    version='0.0.2',
+    author='Ruben Lipperts',
+    author_email='',
+    description='Simple text output formatter that helps with printing pretty stuff',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rlipperts/formatter",
-    packages=setuptools.find_packages(where='src'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rlipperts/formatter',
     package_dir={'': 'src'},
+    packages=['text_formatter'],
+    package_data={'text_formatter': ['py.typed']},
     tests_require=test_deps,
     extras_require=extras,
     install_requires=[
@@ -31,10 +32,10 @@ setuptools.setup(
         'colored',
     ],
     classifiers=[
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Text Processing :: General",
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Text Processing :: General',
     ],
     python_requires='~=3.9',
 )
