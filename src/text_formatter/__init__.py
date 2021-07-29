4c3a88fe5b4d400589155af390c7b4ecec8f2013
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-29 13:10:38 +0200

    Make repository pylint and flake8 conforming

--- src/text_formatter/__init__.py
+++ src/text_formatter/__init__.py
@@ -1,1 +1,5 @@
-from .formatter import box, colorize, colorize_bg, heading, highlight, indent, pretty
+"""
+Provides the top-level load function to users without having to import the loader explicitly.
+Additionally, this hides internal functions of the loader.
+"""
+from .formatter import box, colorize, colorize_bg, heading, highlight, indent, pretty  # NOQA: F401
--- src/text_formatter/formatter.py
+++ src/text_formatter/formatter.py
@@ -1,33 +1,60 @@
+"""
+Implements the formatting functionality.
+Allows styling/formatting of text with custom functions and wrappers for pprintpp and colored.
+"""
 import shutil
 import textwrap
 import pprintpp as pprint
 import colored
 
-indent_size = 4
-highlight_color = 'orange_1'
+INDENT_SIZE = 4
+HIGHLIGHT_COLOR = 'orange_1'
+BOX_CHARACTER = '#'
+
 
 def _terminal_width() -> int:
+    """
+    Get terminal width.
+    :return: Width of the terminal
+    """
     return shutil.get_terminal_size(fallback=(80, 50)).columns
 
 
 def heading(text: str) -> str:
+    """
+    Formats headings, shortcut for box() inside highlight() function.
+    :param text: Text to format
+    :return: Formatted text
+    """
     return highlight(box(text))
 
 
 def highlight(text: str) -> str:
-    return colorize(text, highlight_color)
+    """
+    Highlight text by changing its color to HIGHLIGHT_COLOR
+    :param text: Text to format
+    :return: Formatted text
+    """
+    return colorize(text, HIGHLIGHT_COLOR)
 
 
 def box(text: str) -> str:
-    filled = '#' * _terminal_width()
-    blank = '#' + ' ' * (_terminal_width() - 2) + '#'
-    prefix = "# ====> "
-    suffix = " #"
+    """
+    Wraps text inside a box of BOX_CHARACTER characters to have it stand out of even large
+    amounts of output.
+    :param text: Text to format
+    :return: Formatted text
+    """
+    filled = BOX_CHARACTER * _terminal_width()
+    blank = BOX_CHARACTER + ' ' * (_terminal_width() - 2) + BOX_CHARACTER
+    prefix = BOX_CHARACTER + ' ====> '
+    suffix = ' ' + BOX_CHARACTER
     lines = _split(text, " ", _terminal_width() - len(prefix) - len(suffix))
 
     output = ['', filled, blank]
     for line in lines:
-        output.append(prefix + line + ' ' * (_terminal_width() - len(prefix) - len(line) - len(suffix)) + suffix)
+        output.append(prefix + line + ' ' * (_terminal_width() - len(prefix) - len(line) -
+                                             len(suffix)) + suffix)
     output.append(blank)
     output.append(filled)
     output.append('')
@@ -36,6 +63,13 @@ def box(text: str) -> str:
 
 
 def _split(text: str, sep: str, length: int) -> list[str]:
+    """
+    Split given text into smaller chunks inside given size limit.
+    :param text: Text to split
+    :param sep: Separator at which the text can be split
+    :param length: Maximal length of split text lines
+    :return: Split text lines
+    """
     separated = []
     words = text.split(sep)
     counter = 0
@@ -55,20 +89,48 @@ def _split(text: str, sep: str, length: int) -> list[str]:
 
 
 def _vsep() -> str:
-    return '\n' + '=' * int(_terminal_width() / 2) + '\n\n'
+    """
+    Separator to visibly split text vertically.
+    :return: Line of BOX_CHARACTER
+    """
+    return '\n' + BOX_CHARACTER * int(_terminal_width() / 2) + '\n\n'
 
 
 def pretty(obj: any) -> str:
-    return '\n' + pprint.pformat(obj, indent=indent_size)
+    """
+    Prettyprint a data structure.
+    :param obj: Data structure to print pretty
+    :return: String of pretty data structure representation
+    """
+    return '\n' + pprint.pformat(obj, indent=INDENT_SIZE)
 
 
-def indent(text: str, amount: int=1, ch: str=' ') -> str:
-    return textwrap.indent(text, amount*indent_size*ch)
+def indent(text: str, amount: int = 1, indent_character: str = ' ') -> str:
+    """
+    Indent text.
+    :param text: Text to indent
+    :param amount: Width of indent
+    :param indent_character: Character to use for indent
+    :return: Indented text
+    """
+    return textwrap.indent(text, amount * INDENT_SIZE * indent_character)
 
 
 def colorize(text: str, color: str) -> str:
+    """
+    Colorized given text for colored terminal printing.
+    :param text: Text to colorize
+    :param color: Color to use, see https://pypi.org/project/colored/ for a list of color strings
+    :return: Colorized text
+    """
     return colored.stylize(text, colored.fg(color))
 
 
 def colorize_bg(text: str, color: str) -> str:
+    """
+    Colorized given texts background for colored terminal printing.
+    :param text: Text to colorize
+    :param color: Color to use, see https://pypi.org/project/colored/ for a list of color strings
+    :return: Text with colorized background
+    """
     return colored.stylize(text, colored.bg(color))
